bc1d37e28fbd1aee74e0fd2ecc009a32294bda27
likhity <73198499+likhity@users.noreply.github.com>
2021-06-15 16:53:07 -0700

    Dark Mode support

--- Color Tools/Random Color/script.js
+++ Color Tools/Random Color/script.js
@@ -1,75 +1,77 @@
 function randomColor() {
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
+  var x = Math.floor(Math.random() * 256);
+  var y = Math.floor(Math.random() * 256);
+  var z = Math.floor(Math.random() * 256);
 
-    var color = [x, y, z]
+  var color = [x, y, z];
 
-    var numLessThan127 = 0;
+  var numLessThan127 = 0;
 
-    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-  
-    document.body.style.background = bgColor;
-    document.getElementById('color-rgb').innerHTML = x + ", " + y + ", " + z;
-    document.getElementById('color-hex').innerHTML = rgbToHex(x,y,z);
+  var bgColor = "rgb(" + x + "," + y + "," + z + ")";
 
-    for (var i = 0; i < 3; i++) {
-        if (color[i] < 127) {
-            numLessThan127++;
-        }
+  document.body.style.background = bgColor;
+  document.getElementById("color-rgb").innerHTML = x + ", " + y + ", " + z;
+  document.getElementById("color-hex").innerHTML = rgbToHex(x, y, z);
+
+  for (var i = 0; i < 3; i++) {
+    if (color[i] < 127) {
+      numLessThan127++;
     }
+  }
 
-    if (numLessThan127 > 1) {
-        document.getElementById('color-rgb').style.color = 'white';
-        document.getElementById('color-hex').style.color = 'white';
-        document.getElementById('myH1').style.color = 'white';
-        document.getElementById('rgb').style.color = 'white';
-        document.getElementById('hex').style.color = 'white';
-        document.getElementById('noBull').style.color = 'white';
-    }
-    else {
-        document.getElementById('color-rgb').style.color = 'black';
-        document.getElementById('color-hex').style.color = 'black';
-        document.getElementById('myH1').style.color = 'black';
-        document.getElementById('rgb').style.color = 'black';
-        document.getElementById('hex').style.color = 'black';
-        document.getElementById('noBull').style.color = 'black';
-    }
-} 
+  if (numLessThan127 > 1) {
+    document.getElementById("color-rgb").style.color = "white";
+    document.getElementById("color-hex").style.color = "white";
+    document.querySelector(".myH1").style.color = "white";
+    document.querySelectorAll(".color-type").forEach((element) => {
+      element.style.color = "white";
+    });
+  } else {
+    document.getElementById("color-rgb").style.color = "black";
+    document.getElementById("color-hex").style.color = "black";
+    document.querySelector(".myH1").style.color = "black";
+    document.querySelectorAll(".color-type").forEach((element) => {
+      element.style.color = "black";
+    });
+  }
+}
 
 function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
 }
-      
+
 function rgbToHex(r, g, b) {
-    return "#" + (componentToHex(r) + componentToHex(g) + componentToHex(b)).toUpperCase();
+  return (
+    "#" +
+    (componentToHex(r) + componentToHex(g) + componentToHex(b)).toUpperCase()
+  );
 }
 
 function copyRgb() {
-    selectText('color-rgb');
-    document.execCommand("copy");
+  selectText("color-rgb");
+  document.execCommand("copy");
 }
 
 function copyHex() {
-    selectText('color-hex');
-    document.execCommand("copy");
+  selectText("color-hex");
+  document.execCommand("copy");
 }
 
 function selectText(node) {
-    node = document.getElementById(node);
+  node = document.getElementById(node);
 
-    if (document.body.createTextRange) {
-        const range = document.body.createTextRange();
-        range.moveToElementText(node);
-        range.select();
-    } else if (window.getSelection) {
-        const selection = window.getSelection();
-        const range = document.createRange();
-        range.selectNodeContents(node);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    } else {
-        console.warn("Could not select text in node: Unsupported browser.");
-    }
+  if (document.body.createTextRange) {
+    const range = document.body.createTextRange();
+    range.moveToElementText(node);
+    range.select();
+  } else if (window.getSelection) {
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNodeContents(node);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  } else {
+    console.warn("Could not select text in node: Unsupported browser.");
+  }
 }
---
+++ script.js
@@ -0,0 +1,40 @@
+const themeToggle = document.querySelector(".toggle-theme-checkbox");
+const LOCAL_STORAGE_KEY = "VYASPORTFOLIO";
+
+const localData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || {
+  darkTheme: themeToggle.checked,
+};
+
+changeTheme(localData.darkTheme);
+themeToggle.checked = localData.darkTheme;
+localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(localData));
+
+themeToggle.addEventListener("change", () => {
+  if (themeToggle.checked) {
+    changeTheme(true);
+    localData.darkTheme = true;
+  } else {
+    changeTheme(false);
+    localData.darkTheme = false;
+  }
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(localData));
+});
+
+function changeTheme(darkTheme) {
+  if (darkTheme) {
+    setDocumentProperty("--background-color", "rgb(27, 27, 27)");
+    setDocumentProperty("--color", "white");
+    setDocumentProperty("--box-shadow-color", "rgba(255,255,255,0.1)");
+    setDocumentProperty("--border-width", "1px");
+    return;
+  }
+  setDocumentProperty("--background-color", "white");
+  setDocumentProperty("--color", "black");
+  setDocumentProperty("--border-width", "0");
+}
+
+function setDocumentProperty(property, value) {
+  document.documentElement.style.setProperty(property, value);
+}
+
+themeToggle.checked = localData.darkTheme;
